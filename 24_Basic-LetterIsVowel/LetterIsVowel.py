ef33f832108ff7475eac32d17c2fd53e0ea3f1f4
nanogens <37455949+nanogens@users.noreply.github.com>
2020-04-25 16:03:58 -0400

    Update 2
    updating

---
+++ 24_Basic-LetterIsVowel/LetterIsVowel.py
@@ -0,0 +1,25 @@
+'''
+24. Write a Python program to test whether a passed letter is a vowel or not.
+'''
+
+def is_vowel(char):
+    all_vowels = 'aeiou'
+    return char in all_vowels
+
+def VowelOrNot(char):
+    all_vowels = 'aeiou'
+    #below will return true if vowel if found, False if not
+    if(char in all_vowels != True):
+        print("\nVowel found: " + char)
+    else:
+        print("\nNo vowel found")
+
+VowelOrNot('a')
+VowelOrNot('e')
+VowelOrNot('i')
+VowelOrNot('o')
+VowelOrNot('u')
+
+VowelOrNot('b')
+VowelOrNot('c')
+VowelOrNot('z')
