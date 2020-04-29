5d84f8826608ade732f459eded5a154227bf9159
nanogens <37455949+nanogens@users.noreply.github.com>
2020-04-28 23:45:03 -0400

    update
    update

---
+++ String/5_String-StringFromStringSwapFirst2Chars/StringFromStringSwapFirst2Chars.py
@@ -0,0 +1,11 @@
+'''
+5. Write a Python program to get a single string from two given strings, separated by a space and swap the
+first two characters of each string. Go to the editor
+
+Sample String : 'abc', 'xyz'
+Expected Result : 'xyc abz'
+'''
+
+def StrSwap(str1, str2)
+    strx = str2[0] + str2[1] + str1[2]
+    
