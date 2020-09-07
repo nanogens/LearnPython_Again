f75987faf7d38811cbc6c0935e71ad8ba4b8a331
nanogens <37455949+nanogens@users.noreply.github.com>
2020-09-07 17:07:39 -0400

    Update
    Update

---
+++ Threading/Thread_Try3/Thread_Try3.py
@@ -0,0 +1,73 @@
+import logging
+import threading
+import time
+
+import serial
+
+def thread_function(name):
+    logging.info("Thread %s: starting", name)
+    #time.sleep(2)
+
+    print('Enter your commands below.\r\nInsert "exit" to leave the application.')
+
+    incoming = 1
+    #incomingA='32'
+    #incomingB=b'32'
+
+    packet = bytearray()
+    packet.append(0x41)
+    packet.append(0x42)
+    packet.append(0x43)
+
+    while 1:
+        # get keyboard input
+        # incoming = input(">> ")
+        # Python 3 users
+        # input = input(">> ")
+        if incoming == 'exit':
+            ser.close()
+            # exit()
+            break
+        else:
+            # send the character to the device
+            # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
+            #ser.write(str.encode(incomingA)) #(b'h') #str.encode(incomingA))
+            #ser.to_bytes(0X10)
+            ser.write(packet)
+            #ser.write(incomingB)
+            out = ''
+            # let's wait one second before reading output (let's give device time to answer)
+            time.sleep(1)
+            while ser.inWaiting() > 0:
+                out += ser.read(1)
+
+            if out != '':
+                print(">>") + out
+
+    logging.info("Thread %s: finishing", name)
+
+if __name__ == "__main__":
+    # configure the serial connections (the parameters differs on the device you are connecting to)
+    ser = serial.Serial(
+        port="COM2",
+        baudrate=115200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS
+    )
+
+    ser.isOpen()
+
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    logging.info("Main    : before creating thread")
+    x = threading.Thread(target=thread_function, args=(1,))
+    logging.info("Main    : before running thread")
+    x.start()
+
+    logging.info("Main    : wait for the thread to finish")
+    # x.join()
+    logging.info("Main    : all done")
+
